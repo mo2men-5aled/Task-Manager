6772e5472664164316c09724b4035242d9f789e1
momen <momenk208@gmail.com>
2022-09-26 22:55:10 +0200

    GET,POST,UPDATE,DELETE

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,17 @@
+const { find } = require("../models/task");
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("all items");
+//find({data})
+//used for searching for a taskes with the data passed in the object in the find function
+
+const getAllTasks = async (req, res) => {
+  try {
+    //to get all the tasks in the database
+    const task = await Task.find({});
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -13,16 +23,46 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with ID: ${taskID}` });
+    }
+    res.status(200).json({ msg: "successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
