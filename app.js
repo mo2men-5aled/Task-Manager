9a9d5dd608ebb8334839c54b663201af2eb277be
momen <momenk208@gmail.com>
2022-09-26 19:00:27 +0200

    create routes and controllers

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
+//middleware
+app.use(express.json());
 //routes
 app.get("/hello", (req, res) => {
   res.send("task manager app");
 });
+
+app.use("/api/v1/tasks", tasks);
+// app.get("/api/v1/tasks")          -get all the tasks
+// app.post("/api/v1/tasks")         -create new task
+// app.get("/api/v1/tasks/:id")      -get single task
+// app.patch("/api/v1/tasks:id")     -update task
+// app.delete("/api/v1/tasks:id")    -delete task
 
 const port = 3000;
 
